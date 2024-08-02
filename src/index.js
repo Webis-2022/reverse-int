1a5723bddf51e308ac5e56fba981ed7c6cdbafc5
Webis-2022 <verjovkin.jevgeni@gmail.com>
2024-08-02 17:22:37 +0300

    feat: Make the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const absoluteNum = Math.abs(n);
+    const reversed = absoluteNum.toString().split('').reverse().join('');
+    return Number(reversed);
 }
